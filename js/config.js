a128aa52e47f68340798525cc5318e1923c3bacc
alfiananandaid <alfianananda.id@gmail.com>
2026-09-11 19:57:52 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    // GANTI URL INI DENGAN URL WEB APP DEPLOYMENT GAS ANDA
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbx9ibnM4uNOkgK7aXQHyzRiX9moPIVYTZXBXNqmXQVfLVEtDUdqqYftjxzkbbrdovnxeg/exec',
+    APP_VERSION: '1.0.0',
+    STORAGE_KEY: 'som_session'
+};
